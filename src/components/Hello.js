6230e057f0a3a8aa6a47c16747b15f53931d9b60
Anhar <anhar_tasman@yahoo.com>
2024-04-04 16:47:50 +0700

    React createElement

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Hello = () => {
-  return (
-    <div>
-      <h1>Hello Vishwas</h1>
-    </div>
+  //   return (
+  //     <div>
+  //       <h1>Hello Vishwas</h1>
+  //     </div>
+  //   );
+
+  return React.createElement(
+    "div",
+    { id: "Hello", className: "dummyClass" },
+    React.createElement("h1", null, "Hello Vishwas")
   );
 };
 export default Hello;
