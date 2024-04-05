69304058710d844c4f7e6aed79a48760c216da8c
Anhar <anhar_tasman@yahoo.com>
2024-04-06 02:53:09 +0700

    Destructuring props part 2

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,7 +4,8 @@ import React from "react";
 //   return <h1>Hello user</h1>;
 // }
 
-const Greet = ({ name, heroName }) => {
+const Greet = (props) => {
+  const { name, heroName } = props;
   return (
     <div>
       <h1>
