7d41d638a597987c8bc687941e2896c6c9f67ad7
Anhar <anhar_tasman@yahoo.com>
2024-04-07 06:25:21 +0700

    list with keys

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -22,7 +22,10 @@ function NameList() {
       skill: "Vue",
     },
   ];
-  const personList = persons.map((person) => <Person person={person} />);
+  //key is needed when creating list of elements. Key gives the elements stable identity
+  const personList = persons.map((person) => (
+    <Person key={person.id} person={person} />
+  ));
   return <div>{personList}</div>;
 }
 
