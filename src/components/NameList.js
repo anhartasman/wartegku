8befd7c703615bc59adfcc596c39e39fc62614a4
Anhar <anhar_tasman@yahoo.com>
2024-04-07 06:31:12 +0700

    index as  key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,6 +2,7 @@ import React from "react";
 import Person from "./Person";
 
 function NameList() {
+  const names = ["Bruce", "Clark", "Diana", "Bruce"];
   const persons = [
     {
       id: 1,
@@ -23,8 +24,10 @@ function NameList() {
     },
   ];
   //key is needed when creating list of elements. Key gives the elements stable identity
-  const personList = persons.map((person) => (
-    <Person key={person.id} person={person} />
+  const personList = names.map((name, index) => (
+    <h2 key={index}>
+      {index} {name}
+    </h2>
   ));
   return <div>{personList}</div>;
 }
