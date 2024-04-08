a12065479e94ec08fe2cce4fc19f6e9cd9e3ec88
Anhar <anhar_tasman@yahoo.com>
2024-04-08 11:14:50 +0700

    CSS Modules

--- src/components/Inline.js
+++ src/components/Inline.js
@@ -7,6 +7,10 @@ const heading = {
 function Inline() {
   return (
     <div>
+      {/* CSS class can be applied to child component */}
+      <h1 className="error">Error</h1>
+      {/* CSS Modules cant be applied to child component
+      <h1 className={styles.success}>Success</h1> */}
       <h1 style={heading}>Inline</h1>
     </div>
   );
