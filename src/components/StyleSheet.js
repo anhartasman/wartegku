28b9ece3982d9eb01d33748a11fbfa23df910491
Anhar <anhar_tasman@yahoo.com>
2024-04-08 11:05:27 +0700

    multiple class

--- src/components/StyleSheet.js
+++ src/components/StyleSheet.js
@@ -5,7 +5,7 @@ function StyleSheet(props) {
   let className = props.primary ? "primary" : "";
   return (
     <div>
-      <h1 className={className}>StyleSheet</h1>
+      <h1 className={className + " font-xl"}>StyleSheet</h1>
     </div>
   );
 }
